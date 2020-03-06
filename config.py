5df582bb1098b43a6df34f240779644875f3c8a7
Garrick Suemith <garrick.suemith@gmail.com>
2020-03-06 02:33:06 -0500

    initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+
+
+class Config:
+    SECRET_KEY = '9b402e91e8b5c90fc27e5baf553e848e'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = "garrick.suemith"
+    MAIL_PASSWORD = ""
